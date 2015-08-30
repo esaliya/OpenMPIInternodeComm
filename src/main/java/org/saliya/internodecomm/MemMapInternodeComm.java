e914412a354370e27a231078742cf1895582e17b
Saliya Ekanayake <esaliya@gmail.com>
2015-08-30 15:56:30 -0400

    adding code

--- src/main/java/org/saliya/internodecomm/MemMapInternodeComm.java
+++ src/main/java/org/saliya/internodecomm/MemMapInternodeComm.java
@@ -1,8 +1,11 @@
 package org.saliya.internodecomm;
 
+import mpi.Group;
 import mpi.Intracomm;
 import mpi.MPI;
 import mpi.MPIException;
+
+import java.nio.IntBuffer;
 
 public class MemMapInternodeComm {
     public static void main(String[] args) throws MPIException {
@@ -11,6 +14,34 @@ public class MemMapInternodeComm {
         int worldRank = worldComm.getRank();
         int worldSize = worldComm.getSize();
         System.out.println("Hello from " + worldRank + " of " + worldSize);
+
+        int nodeCount = Integer.parseInt(args[0]);
+        assert worldSize % nodeCount == 0;
+        int worldProcsPerNode = worldSize / nodeCount;
+        // Number of communicating groups per node
+        int cgPerNode = Integer.parseInt(args[1]);
+
+        int worldNodeLocalRank = worldRank % worldProcsPerNode;
+        int q = worldProcsPerNode / cgPerNode;
+        int r = worldProcsPerNode % cgPerNode;
+
+        // Communicating group
+        int cgId = worldNodeLocalRank < r*(q+1) ? worldNodeLocalRank/(q+1) : (worldNodeLocalRank-r)/q;
+        int cgProcCountPerNode = worldNodeLocalRank < r*(q+1) ? q+1 : q;
+        boolean cgLead = worldNodeLocalRank % cgProcCountPerNode == 0;
+
+        Intracomm cgComm = worldComm.split(cgLead ? worldNodeLocalRank : -1, worldRank);
+
+        // Testing
+        if (cgLead) {
+            IntBuffer buff = MPI.newIntBuffer(1);
+            buff.position(0);
+            buff.put(cgComm.getRank());
+            cgComm.allReduce(buff, 1, MPI.INT, MPI.MAX);
+            System.out.println("WorldRank: " + worldRank + " CGRank: " + cgComm.getRank() + " MaxCGRank: " + buff.get(0));
+        }
+
+
         MPI.Finalize();
     }
 }
