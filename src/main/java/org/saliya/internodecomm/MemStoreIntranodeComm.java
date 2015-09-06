e4bff21b1ce18b652eeca4bfc83222876885dc56
Saliya Ekanayake <esaliya@gmail.com>
2015-09-06 11:45:09 -0400

    some debugs

--- src/main/java/org/saliya/internodecomm/MemStoreIntranodeComm.java
+++ src/main/java/org/saliya/internodecomm/MemStoreIntranodeComm.java
@@ -132,7 +132,7 @@ public class MemStoreIntranodeComm {
             for (int i = 0; i < threadRowCount; ++i) {
                 for (int j = 0; j < targetDimension; ++j) {
                     bytes.writeDouble(Math.random());
-                    mmapXWriteBytes.writeDouble(Math.random());
+//                    mmapXWriteBytes.writeDouble(Math.random());
                 }
             }
         }
