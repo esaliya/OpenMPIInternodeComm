ff98cfa5b4df27f8730b8fbddb3283adbdcc09b5
Saliya Ekanayake <esaliya@gmail.com>
2015-09-06 03:22:37 -0400

    some debugs

--- src/main/java/org/saliya/internodecomm/MemStoreMPITest.java
+++ src/main/java/org/saliya/internodecomm/MemStoreMPITest.java
@@ -19,20 +19,20 @@ public class MemStoreMPITest {
         long size = 200;
         long extent = size * count;
         File f = new File(args[0] + "/test.ms");
+        DirectBytes bytes = null;
         try {
             MappedStore ms = new MappedStore(f, FileChannel.MapMode.READ_WRITE, extent<<3);
-            DirectBytes bytes = ms.bytes(size*rank*Double.BYTES, size*Double.BYTES);
-            for (int i = 0; i < size; ++i){
-                bytes.writeDouble(i + Math.random());
-            }
-
-            System.out.println("Came here");
-
-
+            bytes = ms.bytes(size*rank*Double.BYTES, size*Double.BYTES);
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+
+        for (int i = 0; i < size; ++i){
+            bytes.writeDouble(i + Math.random());
+        }
+
+        System.out.println("Came here");
         MPI.Finalize();
     }
 }
