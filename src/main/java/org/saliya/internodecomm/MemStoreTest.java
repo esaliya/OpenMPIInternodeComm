e2ecbe4b5444b22458802ecd57d1bfd9b742999c
Saliya Ekanayake <esaliya@gmail.com>
2015-09-06 02:40:02 -0400

    Adding a memory store test

--- src/main/java/org/saliya/internodecomm/MemStoreTest.java
+++ src/main/java/org/saliya/internodecomm/MemStoreTest.java
@@ -10,7 +10,7 @@ import java.nio.channels.FileChannel;
 public class MemStoreTest {
     public static void main(String[] args) {
         long size = 2000;
-        File f = new File("/dev/shm/test.ms");
+        File f = new File("src/main/resources/test.ms");
         try {
             MappedStore ms = new MappedStore(f, FileChannel.MapMode.READ_WRITE, size<<3);
             DirectBytes bytes = ms.bytes();
@@ -18,6 +18,7 @@ public class MemStoreTest {
                 bytes.writeDouble(i + Math.random());
             }
 
+            System.out.println("Came here");
 
 
         }
