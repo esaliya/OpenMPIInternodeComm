cc01bcb184e7ed9609f982ad8042dcbaf7d61894
Saliya Ekanayake <esaliya@gmail.com>
2015-09-06 02:05:58 -0400

    Adding a memory store test

---
+++ src/main/java/org/saliya/internodecomm/MemStoreTest.java
@@ -0,0 +1,27 @@
+package org.saliya.internodecomm;
+
+import net.openhft.lang.io.DirectBytes;
+import net.openhft.lang.io.MappedStore;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+public class MemStoreTest {
+    public static void main(String[] args) {
+        long size = 2000;
+        File f = new File("/dev/shm/test.ms");
+        try(MappedStore ms = new MappedStore(f, FileChannel.MapMode.READ_WRITE, size<<3)) {
+            DirectBytes bytes = ms.bytes();
+            for (int i = 0; i < size; ++i){
+                bytes.writeDouble(i + Math.random());
+            }
+
+
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
