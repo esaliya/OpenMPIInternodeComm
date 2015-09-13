a6d922af629c67da77f85700bf2866a85371a29d
Saliya Ekanayake <esaliya@gmail.com>
2015-09-13 12:28:03 -0400

    adding onesided example

---
+++ src/main/java/org/saliya/internodecomm/OneSidedComm.java
@@ -0,0 +1,64 @@
+package org.saliya.internodecomm;
+
+import edu.indiana.soic.spidal.common.Range;
+import edu.indiana.soic.spidal.common.RangePartitioner;
+import mpi.Intracomm;
+import mpi.MPI;
+import mpi.MPIException;
+import mpi.Win;
+import net.openhft.lang.io.ByteBufferBytes;
+import net.openhft.lang.io.Bytes;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class OneSidedComm {
+    static Bytes bytes;
+    static ByteBuffer byteBuffer;
+    public static void main(String[] args) throws MPIException {
+        args = MPI.Init(args);
+        Intracomm worldProcComm = MPI.COMM_WORLD;
+        int worldProcRank = worldProcComm.getRank();
+        int worldProcsCount = worldProcComm.getSize();
+
+        int size = Integer.parseInt(args[0]);
+        Range[] ranges = RangePartitioner.partition(size, worldProcsCount);
+        Range myRange = ranges[worldProcRank];
+
+        String fname = "/dev/shm/mmap.tmp";
+        try (FileChannel fc = FileChannel.open(Paths.get(fname),
+                                                    StandardOpenOption.CREATE,StandardOpenOption.WRITE,StandardOpenOption.READ)) {
+            int extent = size*Double.BYTES;
+            bytes = ByteBufferBytes.wrap(fc.map(
+                FileChannel.MapMode.READ_WRITE, 0L,
+                extent));
+            byteBuffer = bytes.sliceAsByteBuffer(byteBuffer);
+            Win win = new Win(byteBuffer, extent, Double.BYTES, MPI.INFO_NULL, worldProcComm);
+
+
+            for (int i = 0; i < myRange.getLength(); ++i){
+                byteBuffer.putDouble(i*Double.BYTES, worldProcRank);
+            }
+            win.fence(0);
+            if (worldProcRank != 0){
+                win.put(byteBuffer, size, MPI.DOUBLE, 0, myRange.getStartIndex(), myRange.getLength(), MPI.DOUBLE);
+            }
+            win.fence(0);
+
+            if (worldProcRank == 0){
+                for (int i = 0; i < size; ++i) {
+                    System.out.println(byteBuffer.getDouble(i*Double.BYTES));
+                }
+            }
+            worldProcComm.barrier();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        MPI.Finalize();
+    }
+}
